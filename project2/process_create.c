3adcc3aff1b794bb794ded6cee68ecabf041261b
7HE-LUCKY-FISH <bryanlin1968@gmail.com>
2025-02-26 20:47:28 -0800

    update of project2 to fit requirements

--- project2/process_create.c
+++ project2/process_create.c
@@ -10,36 +10,34 @@ void sigint_handler(int sig){
 }
 
 int main(void) {
+    signal (SIGINT, SIG_IGN);//ignores the signal so it doesn't kill the parent process as well
     pid_t pid = fork();//create a child process
     
     if (pid < 0) { 
+        fprintf(stderr, "Fork Failed");//check if the fork has failed
         exit(EXIT_FAILURE);//check if the fork has failed and exits with error 
     }
     
     if (pid == 0) {
         signal(SIGINT, sigint_handler);//signal handler for the child process
-        //printf("Child Process PID: %d\n", getpid()); debug statment 
+        printf("Child Process PID: %d\n", getpid()); 
+        fflush(stdout);
         pause();
         exit(5);
     } else {
         int status;
-        kill(pid, SIGINT); //sends a signal to the child process
-
-
-        pid_t child_pid = wait(&status);//waits for the child process to terminate and grabs pid
+        pid_t child_pid = wait(&status);//waits for the child process to finish
         int exitstatus = 0;
+
+
        if (WIFEXITED(status)) {//checks the exit status of the child process
             exitstatus = WEXITSTATUS(status);
             printf("childpid=%d,exitstatus=%d\n", child_pid, exitstatus);
-        } else if (WIFSIGNALED(status)) {
-            printf("childpid=%d terminated by signal %d\n", //checks if the child process was terminated by a signal
-                child_pid, WTERMSIG(status));
-            return EXIT_FAILURE;
-        } else {
-            printf("childpid=%d terminated abnormally\n", child_pid);
+        }
+        else {
+            fprintf(stderr, "childpid=%d terminated abnormally\n", child_pid);
             return EXIT_FAILURE;//if the child process was terminated abnormally
         }
     }
-    
     return 0;
 }
